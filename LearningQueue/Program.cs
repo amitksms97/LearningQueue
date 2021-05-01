eb30ade588bb052a5f8dfda58279205e231c18f9
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:23:23 +0530

    Add project files.

---
+++ LearningQueue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> myQueue = new Queue<int>();
+
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+
+            foreach(var item in myQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Peek());
+        }
+    }
+}
